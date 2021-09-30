4c2904dff40dd6345ea3605839535f3ea00257b9
Akash Pandey <akashpandey242002@gmail.com>
2021-09-30 23:13:47 +0530

    readme.2
    i changed the bg colour of the text

--- digitalclock.py
+++ digitalclock.py
@@ -5,7 +5,7 @@ from datetime import datetime
 window = Tk()
 window.title("digital clock")
 window.geometry("600x600")
-window.configure(bg="steelblue")
+window.configure(bg="red")
 label = Label(window, font=("aerial darkblack ",
               79, 'bold'), bg="steelblue", fg="white")
 label.pack(pady=100)
